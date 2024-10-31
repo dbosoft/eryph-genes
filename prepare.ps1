4437e7911f53221d6e741b07c16c79da7c721828
Frank Wagner <frank2568@gmail.com>
2024-10-31 11:59:55 +0100

    install eryph components

--- prepare.ps1
+++ prepare.ps1
@@ -1,2 +1,7 @@
 
+param ($EMail, $InvitationCode)
+
 install-module powershell-yaml -Force
+
+Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/eryph-org/eryph/main/src/apps/src/Eryph-zero/install.ps1) } -EMail $EMail -InvitationCode $InvitationCode"
+Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/eryph-org/dotnet-genepoolclient/refs/heads/main/src/apps/src/eryph-packer/install.ps1) } -Force -EMail $EMail -InvitationCode $InvitationCode"
