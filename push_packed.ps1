3c4f3c8772513409dfb9474422e7145eef157713
Frank Wagner <frank2568@gmail.com>
2025-03-03 10:09:54 +0100

    exclude next versions from push_packed script

--- push_packed.ps1
+++ push_packed.ps1
@@ -14,13 +14,19 @@ $apikey = $env:ERYPH_APIKEY
 $changedGenesets | ForEach-Object {
 
     $changedGeneset = $_
+
+    if($changedGeneset.EndsWith("next")){
+        Write-Host "Skipping 'next' geneset tag '$changedGeneset'"
+        return
+    }
+
     
     Write-Host "Pushing geneset-tag $changedGeneset"
 
     if($apikey){
         Write-Host "Using API-Key for authentication"
-        & eryph-packer geneset-tag push $changedGeneset --workdir genes --api-key $apikey
+       & eryph-packer geneset-tag push $changedGeneset --workdir genes --api-key $apikey
     } else {
-        & eryph-packer geneset-tag push $changedGeneset --workdir genes
+       & eryph-packer geneset-tag push $changedGeneset --workdir genes
     }
 }
